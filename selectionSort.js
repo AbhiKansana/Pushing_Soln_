028cdc1140b8241708fe6685f8272f4ff4b81d0a
AbhiKansana <abhikansana05@gmail.com>
2022-06-04 16:39:30 +0530

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -2,19 +2,14 @@
 let arr = [5,2,1,3,7,3]
 let n = arr.length
 for(let i=0; i<n-1; i++){
-let cnt = 0
-let minInd = null
+let minInd = i
   for(let j= i+1; j<n; j++){
-    if(arr[i]>arr[j]){
-    cnt++
-      if(cnt===1){ minInd = j}
-      else if(arr[minInd]>arr[j]){
-           minInd = j
-       }
-      
+    if(arr[minInd]>arr[j]){
+         minInd = j
+
     }
   }
-  if(minInd){
+  if(minInd !== i){
     let temp = arr[i]
      arr[i] = arr[minInd]
      arr[minInd] = temp
