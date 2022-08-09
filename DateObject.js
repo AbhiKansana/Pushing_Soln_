1f651ed135fbe58aa139ae57d093b6161a1690db
Abhishek Kansana <100181581+AbhiKansana@users.noreply.github.com>
2022-08-09 20:12:12 +0530

    Create DateObject.js

---
+++ DateObject.js
@@ -0,0 +1,29 @@
+
+let date = new Date()
+
+
+
+let day = date.getDate()
+if(day<10){
+  day = `0${day}`
+}
+// console.log(day)
+
+let month = date.getMonth()+1
+if(month<10){
+  month = `0${month}`
+}
+// console.log(month)
+
+let year = date.getFullYear()
+year = year.toString()
+year = year[2]+year[3]
+// console.log(year)
+
+console.log(`${day}/${month}/${year}`)
+
+
+// Time String
+let time = date.()
+console.log(time)
+
