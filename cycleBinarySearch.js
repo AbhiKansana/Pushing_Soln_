e75bf9c204309db3768b818cf1287db64539f235
Abhishek Kansana <100181581+AbhiKansana@users.noreply.github.com>
2022-08-30 20:54:38 +0530

    Update cycleBinarySearch.js

--- cycleBinarySearch.js
+++ cycleBinarySearch.js
@@ -1,84 +1,38 @@
-//Enter code here
-//Enter code here
-function runProgram(input){
-    input = input.split("\n")
-    let[N,K] = input[0].split(" ").map(Number)
-    let arr  = input[1].split(" ").map(Number)
-    let x1 = logic(N,K,arr)
-    console.log("Index", x1)
-     console.log("No.", arr[x1])
+function logic(n,k,arr){
+//   console.log(N,K,arr)
     
+    let low = 0
+    let high = n-1
     
-}
-
-function logic(N,K,arr){
-    let s = 0
-    let e = arr.length-1
-    while(s<=e){
-        let m = Math.floor(s+(e-s)/2)
-        if(arr[m]>arr[m-1] && arr[m]>arr[m+1]){
-             return m
+    while(low<=high){
+        let mid = Math.floor(low+(high-low)/2)
+        if(arr[mid]===k){
+            return mid
         }
         
-        else if(arr[m]<arr[m-1] && arr[m]<arr[m+1]){
-             return m-1
-            
-        }
-        
-        else if(arr[m]>arr[m-1] && arr[m]<arr[m+1]){
-              if(arr[m]>arr[0]){
-                  s= m+1
-              }
-        }
-        
-         else if(arr[m]>arr[m-1] && arr[m]<arr[m+1]){
-              if(arr[m]<arr[0]){
-                 e = m-1
-              }
-        }
-        
-        else if(e-s ===1){
-            if(arr[m]>arr[m+1]){
-                return m
+        else if(arr[low]<=arr[mid]){
+            if(k>=arr[low] && k<=arr[mid]){
+                high = mid-1
             }
             else{
-                return m+1
+                low = mid+1
             }
         }
+        
+        else{
+            if(k>=arr[mid] && k<=arr[high]){
+                low = mid+1
+            }
+            else{
+                high = mid-1
+            }
+        }
+    }
+    
+    return -1
+        
         
        
         
-    }
-    
-    console.log("-1")
-}
-
  
-
-if (process.env.USER === "") {
-  runProgram(``);
-} else {
-  process.stdin.resume();
-  process.stdin.setEncoding("ascii");
-  let read = "";
-  process.stdin.on("data", function (input) {
-    read += input;
-  });
-  process.stdin.on("end", function () {
-    read = read.replace(/\n$/, "");
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-  });
-  process.on("SIGINT", function () {
-    read = read.replace(/\n$/, "");
-    runProgram(read);
-    process.exit(0);
-  });
-
 }
-
-
- 
-
-
-
